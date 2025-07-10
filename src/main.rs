0bc0902448c6343545c047bf33ee719aee6e40ca
Ziyu <ziyuwang123456@gmail.com>
2025-07-10 09:54:53 -0400

    Implement basic Iced application structure with exit functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{widget::{button, column, text}, window, Element, Task};
+
+#[derive(Debug, Default)]
+struct AppState {
+    // Define the state of your application here
+}
+#[derive(Debug,Clone)]
+enum Message {
+    Exit
+}
+
+fn update(state: &mut AppState, message:Message) -> Task<Message> {
+    match message {
+        Message::Exit => window::get_latest().and_then(window::close), // Close the window when the Exit message is received
+    }
+}
+    
+
+fn view(state: &AppState) ->Element<Message> {
+    column![
+        text("Hello From Application").size(24),
+        button(text("Exit").size(24)).on_press(Message::Exit)
+    ].into() //return an Element containing the UI components
+    
+}
+
+fn main() ->iced::Result {
+    iced::application("Jrip", update, view)
+    .theme(|_s| iced::Theme::KanagawaDragon)
+    .run()
 }
